532c55ce2ac4906100e049fd6eb720ba9eeedb73
小笠原 奈々 <shiningcureseven@gmail.com>
2017-10-24 15:12:42 +0900

    回転と拡大

---
+++ rota-engage.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+GRAY_img *roen(GRAY_img *img_in) ;
+GRAY_img *roen(GRAY_img *img_in){
+  GRAY_img *img_out;
+  img_out = allocGrayimg(img_in -> cols, img_in -> rows);
+
+  //背景を白に初期化
+  for(int i = 0; i < img_out -> cols; i++){
+    for(int j = 0; j < img_out -> rows; j++){
+      img_out -> p[j][i] = 255;
+    }
+  }
+
+  // 回転
+  int x0 = 50;
+  int y0 = 50;
+  int rad = 45;
+  float rx = 2.3;
+  float ry = 2.3;
+  for(int i = 0;i <( img_out -> cols);i++){
+     for(int j = 0;j <( img_out -> rows);j++){
+        int X = (int)(((i-x0)*cos(rad)) - (int)((j-y0)*sin(rad)) + x0)/rx;
+        int Y = (int)(((i-x0)*sin(rad)) + (int)((j-y0)*cos(rad)) + y0)/ry;
+        if( X<0 || Y<0 || X >= img_out -> cols || Y >= img_out -> rows ){
+           img_out -> p[j][i] = 255;
+        }else{
+      　img_out -> p[j][i] = img_in -> p[(int)(floor(Y))][(int)(floor(X))];
+        }
+     }
+}
+  return img_out;
+  }
