3621b6a48a2369c64c0cf5eae8b173c027b2cca0
小笠原 奈々 <shiningcureseven@gmail.com>
2017-10-24 15:08:12 +0900

    回転と拡大(関数化なし)

--- sample.c
+++ sample.c
@@ -7,7 +7,7 @@
 // プロトタイプ宣言
 // #include "mv.c"
 // #include "enlarge.c"
-#include "rotation.c"
+// #include "rotation.c"
 
 #ifdef __STDC__
 int
@@ -40,7 +40,23 @@ main( argc, argv )
 
 
   // 処理
-  img_out = rotation(img_in);
+  int x0 = 50;
+  int y0 = 50;
+  int rad = 45;
+  float rx = 2.3;
+  float ry = 2.3;
+  for(int i = 0;i <( img_out -> cols);i++){
+     for(int j = 0;j <( img_out -> rows);j++){
+        int X = (int)(((i-x0)*cos(rad)) - (int)((j-y0)*sin(rad)) + x0)/rx;
+        int Y = (int)(((i-x0)*sin(rad)) + (int)((j-y0)*cos(rad)) + y0)/ry;
+        if( X<0 || Y<0 || X >= img_out -> cols || Y >= img_out -> rows ){
+           img_out -> p[j][i] = 255;
+        }else{
+      　img_out -> p[j][i] = img_in -> p[(int)(floor(Y))][(int)(floor(X))];
+        }
+     }
+  }
+
 
 
 
